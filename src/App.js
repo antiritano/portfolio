a106a0a4b013c029e52a95b6fdd5a827d99dafbf
Anthony Iritano <81189148+antiritano@users.noreply.github.com>
2022-11-03 10:18:33 -0400

    Adds pong

--- src/App.js
+++ src/App.js
@@ -2,15 +2,16 @@ import './App.css';
 import Hero from './components/Hero/Hero';
 import Layout from './components/Layout/Layout';
 import useScrollSnap from 'react-use-scroll-snap';
-import React, { useEffect, useRef } from 'react';
+import React, { useRef } from 'react';
 import About from './components/About/About';
 import Experience from './components/Experience/Experience';
+import Pong from './components/Pong/Pong';
 
 function App() {
   const scrollRef = useRef(null);
   const activeScroll = useScrollSnap({
     ref: scrollRef,
-    duration: 10,
+    duration: 20,
     delay: 0,
   });
   return (
@@ -20,6 +21,7 @@ function App() {
           <Hero activeScroll={activeScroll} />
           <About activeScroll={activeScroll} />
           <Experience activeScroll={activeScroll} />
+          <Pong />
         </section>
       </Layout>
     </div>
